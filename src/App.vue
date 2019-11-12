35b59c8e7b1e4c8b7f7d93a6fdd87fd77fb9a61b
gyj <1401357358@qq.com>
2019-11-12 20:25:51 +0800

    cubeUI的登录form表单渲染完成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,91 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <cube-button>
-      </cube-button>
+      -->
+      <!--cube-ui的form表单-->
+      <cube-form
+        :model="model"
+        :schema="schema"
+        :immediate-validate="false"
+        :options="options"
+        @validate="validateHandler"
+        @submit="handleLogin"
+        @reset="handleValidate"
+      ></cube-form>
     </div>
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      model: {
+        username: "",
+        password: ""
+      },
+      schema: {
+        fields: [
+          {
+            type: 'input',
+            modelKey: 'username',
+            label: '用户名',
+            props: {
+              placeholder: '请输入'
+            },
+            rules: {
+              required: true
+            },
+            trigger: 'blur'
+          },
+          {
+            type: 'input',
+            modelKey: 'password',
+            label: '密码',
+            props: {
+              type: 'password',
+              placeholder: '请输入',
+              eye: {
+                open: true
+              }
+            },
+            rules: {
+              required: true
+            },
+            trigger: 'blur'
+          },
+          {
+            type: 'submit',
+            label: '登录'
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleLogin(e) {
+      e.preventDefault();
+      console.log("submit", e);
+    },
+    handleValidate(result) {
+      this.validity = result.validity;
+      this.valid = result.valid;
+      console.log(
+        "validity",
+        result.validity,
+        result.valid,
+        result.dirty,
+        result.firstInvalidFieldIndex
+      );
+    }
+  }
+};
+</script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,27 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: '/home',
     component: Home
+  },
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  // login页面被保护，通过meta标识。meta为true则表示被保护。
+  // 被保护的组件访问时必须登录后才能访问,也就是说，访问时必须携带token。。
+  {
+    path: '/login',
+    component: () => import('../views/Login.vue')
+
   },
   {
     path: '/about',
     name: 'about',
+    // meta表示此组件被保护。
+    meta: {
+      auth: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -24,4 +38,27 @@ const router = new VueRouter({
   routes
 })
 
+// 全局路由守卫，
+router.beforeEach((to, from, next) => {
+  // 如果要跳转的组件需要token认证。
+  if (to.meta.auth) {
+    // 获取token，
+    let token = localStorage.getItem('token')
+    // 如果没有token值
+    if (!token) {
+      next({
+        // 跳转到login页。因为需要登录,只有登录才能获取token。
+        path: '/login',
+        query: {redirect: to.path}
+      })
+    } else {
+      next()
+    }
+  }else{
+    next()
+  }
+  
+
+
+})
 export default router
